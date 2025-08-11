66d3b130bb0a53d53b730a4c73ed896534c5478f
kokko <kota.mysa6505@gmail.com>
2025-08-11 14:12:18 +0900

    import React in each components

---
+++ src/components/ResultScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResultScreen: React.FC = () => {
+  return null;
+};
+
+export default ResultScreen;
---
+++ src/components/TitleScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TitleScreen: React.FC = () => {
+  return null;
+};
+
+export default TitleScreen;
