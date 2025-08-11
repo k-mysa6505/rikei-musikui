22d618d407f660a78d444ce5fde2cd7adfac13f8
kokko <kota.mysa6505@gmail.com>
2025-08-11 16:07:14 +0900

    add description in HowToPlayScreen.tsx

--- src/components/HowToPlayScreen.tsx
+++ src/components/HowToPlayScreen.tsx
@@ -9,6 +9,13 @@ const HowToPlayScreen: React.FC<HowToPlayScreenProps> = ({ onOK, onBack }) => {
   return (
     <div className="how-to-play-screen">
       <h1>あそびかた</h1>
+      <ol>
+        <li>数式の□に入る数字を答えてね</li>
+        <li>答えは整数の範囲だよ</li>
+        <li>全7問あるよ</li>
+        <li>Sランク目指してがんばってね</li>
+        <li>負数は「-」先に押してね</li>
+      </ol>
       <button onClick={onOK}>OK</button>
       <button onClick={onBack}>もどる</button>
     </div>
